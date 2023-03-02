dfae986a70d1289fe103042643ebb05189e3b6af
Jesus Garland <jgarlandh@gmail.com>
2023-03-01 21:54:10 -0300

    Creacion clase Persona

--- pruebajava/src/main/java/com/genjavatest/pruebajava/PruebajavaApplication.java
+++ pruebajava/src/main/java/com/genjavatest/pruebajava/PruebajavaApplication.java
@@ -1,13 +1,9 @@
 package com.genjavatest.pruebajava;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class PruebajavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PruebajavaApplication.class, args);
 	}
 
 }
